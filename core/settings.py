90e5e04703d1c1ea4c45eb7a8d94e462bb3c88f6
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-11-06 13:56:54 +0100

    Delete duplicates in settings.py

--- core/settings.py
+++ core/settings.py
@@ -81,20 +81,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-CSRF_TRUSTED_ORIGINS = [
-    'http://localhost:4200',
-    'http://127.0.0.1:4200',
-    'http://127.0.0.1:5500',
-    'http://127.0.0.1:5501',
-]
-
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:4200',
-    'http://127.0.0.1:4200',
-    'http://127.0.0.1:5500',
-    'http://127.0.0.1:5501',
-]
-
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
